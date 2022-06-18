2971f2e19198ba0c1b92ee03ab4d6c7d691fbde0
lxx <949987366@qq.com>
2022-06-18 20:48:53 +0800

    new 15

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -6,6 +6,6 @@ if (process.env.NODE_ENV==='development'){
 }else if (process.env.NODE_ENV==='production'){
   BASE_URL='/api'
 }else {
-  BASE_URL='http://123.207.32.32:8000/'
+  BASE_URL='/api'
 }
 export {BASE_URL,TIME_OUT}
