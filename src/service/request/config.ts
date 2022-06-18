963cb3bd9c5aa5a561a5dd0eade3dde27004f652
lxx <949987366@qq.com>
2022-06-18 14:34:08 +0800

    new 11

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV==='development'){
   // BASE_URL='http://123.207.32.32:8000/home/multidata'
   BASE_URL='/api'
 }else if (process.env.NODE_ENV==='production'){
-  BASE_URL='http://123.207.32.32:8000/'
+  BASE_URL='http://152.136.185.210:5000/'
 }else {
   BASE_URL='http://152.136.185.210:5000/'
 }
--- vue.config.js
+++ vue.config.js
@@ -17,7 +17,7 @@ module.exports = defineConfig({
     //解决跨域访问
     proxy:{
       '/api':{
-        target:'http://152.136.185.210:5000',
+        target:'http://152.136.185.210:5000/',
         pathRewrite:{
           '/api':''
         },
